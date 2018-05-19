6e692845bd24201e2f762fea1e78b28d01d17f6a
github username <github email address>
2018-05-19 12:15:58 -0400

    implemented anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(testers)
+    testers.select { |string| @anagram.split("").sort{|l1, l2| l1<=>l2}.join ==
+     string.split("").sort{|l1, l2| l1<=>l2}.join }
+  end
+end
